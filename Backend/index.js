3c128f1296f82da8165969d67289f0835cca544e
Harsh Thakur <thakurharsh345@gmail.com>
2024-12-05 10:59:38 +0530

    create folder for sih 2024

---
+++ Backend/index.js
@@ -0,0 +1,21 @@
+
+const express = require("express");
+const app = express();
+const bodyparser = require("body-parser");
+app.use('/public', express.static('public'));
+const cors = require("cors");
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({ extended: true }));
+app.use(
+  cors({
+    credentials: true,
+    methods: ["POST", "GET", "PUT", "DELETE"],
+    origin: "http://localhost:5173",
+  })
+);
+
+
+const Port = process.env.Port || 3000;
+app.listen(Port, () => {
+  console.log("Server is running on port " + Port);
+});
